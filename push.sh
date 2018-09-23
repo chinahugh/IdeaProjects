6176d04375223c13c21f123b8014850935b11c38
HUGH <18809491104@163.com>
2018-09-23 15:33:08 +0800

    springcloud

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #! /bin/bash 
 echo start push...
 echo "***************************************************************************">>push.log
+echo "message >> $1">>push.log
 date >>push.log
 git add . >>push.log 2>&1
 git commit -m $1 >>push.log 2>&1
