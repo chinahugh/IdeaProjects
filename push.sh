d5a06cf5d74cc30571e76594b6ff86951a385a24
HUGH <18809491104@163.com>
2018-09-23 15:41:48 +0800

    asdasdasdasdasdasdasdas

--- push.sh
+++ push.sh
@@ -1,8 +1,9 @@
 #! /bin/bash 
+read -t 36000  -p "请输入信息" msg
 echo start push...
 echo "***************************************************************************">>push.log
 date >>push.log
-echo "message >>>>> $1">>push.log
+echo "message >>>>> $msg">>push.log
 git add . >>push.log 2>&1
-git commit -m $1 >>push.log 2>&1
+git commit -m $msg >>push.log 2>&1
 git push origin master >>push.log 2>&1
