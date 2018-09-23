2a5a6d21f80cac8ac4f26d2bdad8349e30e23c39
HUGH <18809491104@163.com>
2018-09-23 15:29:28 +0800

    springcloud

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #! /bin/bash 
 echo start push...
 date >>push.log
-git add . >>push.log
-git commit -m $1 >>push.log
-git push origin master >>push.log
+git add . >>push.log 2>&1
+git commit -m $1 >>push.log 2>&1
+git push origin master >>push.log 2>&1
