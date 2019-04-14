039148081dc30be5eeba9ff06d142a42b2fb9f37
huguohui1314 <18809491104@163.com>
2019-04-14 10:50:42 +0800

    test

--- push.sh
+++ push.sh
@@ -1,16 +1,19 @@
 #! /bin/bash 
 
-read -t 36000  -p "请输入本次提交信息 " msg
-echo start push...
 echo "******************************************************************************************************">>push.log
+echo start push...
+read -t 36000  -p "请输入本次提交信息 " msg
 date >>push.log
 echo "message >>>>> $msg">>push.log
 git add . >>push.log 2>&1
 git commit -m "$msg" >>push.log 2>&1
 git push origin master >>push.log 2>&1
 
+echo "******************************************************************************************************"
 # 备份mysql数据库
 # 备份文件在同级目录下的mysql.sql文件中
 # 向数据库导入备份文件时，先删除文件中的mysq数据库
 echo "开始备份数据库..."
-mysqldump -uroot -proot -A >mysql.sql
+"$MYSQL_HOME/bin/mysqldump.exe" -uroot -proot -A >mysql.sql
+echo "备份数据库完成..."
+read -t 36000
